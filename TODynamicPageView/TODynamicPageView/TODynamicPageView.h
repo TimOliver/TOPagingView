50382fe8f596fec1a7ca5b80f87c35026ba35005
Tim Oliver <me@timoliver.com.au>
2020-03-24 22:11:06 +0800

    Added page view class

---
+++ TODynamicPageView/TODynamicPageView/TODynamicPageView.h
@@ -0,0 +1,17 @@
+//
+//  TODynamicPageView.h
+//  TODynamicPageViewExample
+//
+//  Created by Tim Oliver on 2020/03/24.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TODynamicPageView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TODynamicPageView/TODynamicPageView/TODynamicPageView.m
@@ -0,0 +1,67 @@
+//
+//  TODynamicPageView.m
+//  TODynamicPageViewExample
+//
+//  Created by Tim Oliver on 2020/03/24.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+#import "TODynamicPageView.h"
+
+@interface TODynamicPageView ()
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+@end
+
+@implementation TODynamicPageView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) { [self setUp]; }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) { [self setUp]; }
+    return self;
+}
+
+- (void)setUp
+{
+    // Configure the main properties of this view
+    self.clipsToBounds = YES;
+    
+    // Create the scroll view
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
+    self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.scrollView addObserver:self forKeyPath:@"contentOffset" options:0 context:nil];
+    [self addSubview:self.scrollView];
+}
+
+- (void)didMoveToSuperview
+{
+    [super didMoveToSuperview];
+    
+    CGRect frame = self.frame;
+    CGSize contentSize = self.bounds.size;
+    contentSize.width
+}
+
+- (void)scrollViewDidScroll
+{
+    NSLog(@"%@", NSStringFromCGPoint(self.scrollView.contentOffset));
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                       context:(void *)context
+{
+    [self scrollViewDidScroll];
+}
+
+@end
