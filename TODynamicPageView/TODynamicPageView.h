1db52c7a2824bcbe2b97fb91adeb66c08294d3bf
Tim Oliver <me@timoliver.com.au>
2020-03-27 00:16:04 +0800

    Updated header and added license

--- TODynamicPageView/TODynamicPageView.h
+++ TODynamicPageView/TODynamicPageView.h
@@ -1,10 +1,24 @@
 //
 //  TODynamicPageView.h
-//  TODynamicPageViewExample
 //
-//  Created by Tim Oliver on 2020/03/24.
-//  Copyright © 2020 Tim Oliver. All rights reserved.
+//  Copyright 2018-2020 Timothy Oliver. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
@@ -147,6 +161,12 @@ typedef enum {
 
 /** Advance one page to the right (Regardless of current scroll direction) */
 - (void)turnToRightPageAnimated:(BOOL)animated;
+
+/** Jump ahead to an arbitry next page view, using the provided block to generate the page. */
+- (void)jumpToNextPageWithBlock:(UIView * (^)(TODynamicPageView *dynamicPageView, UIView *currentView))pageBlock;
+
+/** Jump backwards to an arbitrary previous page view, using the provided block to generate the page. */
+- (void)jumpToPreviousPageWithBlock:(UIView * (^)(TODynamicPageView *dynamicPageView, UIView *currentView))pageBlock;
 
 @end
 
